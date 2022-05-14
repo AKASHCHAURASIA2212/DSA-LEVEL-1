ae332380aa8ecb2d34b3e97036cb2eec45c26822
AKASH CHAURASIA <AKASHCHAURASIA2212@gmail.com>
2022-05-15 00:14:36 +0530

    added

---
+++ 12.TREE/LC_94/Solution.java
@@ -0,0 +1,46 @@
+public class Solution {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public static List<Integer> inorder(TreeNode root,List<Integer> ans)
+ {
+     if(root==null)
+     {
+       return ans;
+     }
+    
+     if(root.left!=null)
+     {
+         inorder(root.left,ans);        
+     }
+        
+     ans.add(root.val);
+        
+     if(root.right!=null)
+     {
+         inorder(root.right,ans);        
+     }
+     return ans;
+     
+ }
+ public List<Integer> inorderTraversal(TreeNode root) {
+     List<Integer> ans = new ArrayList<>();
+     List<Integer> myans = inorder(root,ans);
+     return myans;
+ }
+}
+}
